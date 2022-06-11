4f7f4ae1f34a98bd27cadf5799ec775499280cf0
Raper-programistagit <Damian.Siadaczka@hotmail.com>
2022-06-11 16:21:05 +0200

    change app.js

--- app.js
+++ app.js
@@ -1,10 +1,72 @@
 const slide = document.querySelector('.slide');
 const image = document.querySelectorAll('.slide img');
 
-//Button
-const previousButton = document.querySelector('#previousy-button');
+//Images
+const prevImage = document.querySelector('#previously-slide');
+const centerImage = document.querySelector('#slide-center');
+const nextImage = document.querySelector('#next-slide');
+
+//Buttons
+const prevButton = document.querySelector('#previousy-button');
 const nextButton = document.querySelector('#next-button');
 
+const LAST = 5;
+let counter = 1;
+
+function createDiv(numImage) {
+
+    if(numImage)
+
+    return(
+        '<div class="slide-center">' +
+            '<img src="./img/cars/' +numImage+ '.png" alt="">' +
+        '</div>'
+    )
+}
+
+function prevNum(num) {
+
+    if(num === 1)
+        return LAST;
+    else
+        return --num;
+}
+
+function nextNum(num) {
+
+    if(num === LAST)
+        return 1
+    else
+        return ++num;
+}
+
+function changeDivs() {
+
+    const prev = prevNum(counter);
+    const next = nextNum(counter);
+
+    prevImage.innerHTML = createDiv(prev);
+    centerImage.innerHTML = createDiv(counter);
+    nextImage.innerHTML = centerImage(next);
+
+}
+
+prevButton.addEventListener('click', () => {
+
+    counter = prevNum(counter);
+    changeDivs();
+    
+})
+
+nextButton.addEventListener('click', () => {
+
+    counter = nextNum(counter);
+    changeDivs()
+})
+
+
+
+/*
 let counter = 1;
 const size = image[0].clientWidth;
 slide.style.transform = 'translateX(' + (-size * counter ) + 'px)';
@@ -37,4 +99,4 @@ slide.addEventListener('transitionend', () => {
         counter = image.length - counter;
         slide.style.transform = 'translateX(' + (-size * counter) + 'px)';
     }
-})
+})*/
